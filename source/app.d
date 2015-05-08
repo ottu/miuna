ff7c64a3a632f838c2a2ae665e06145e56837962
ottu <ottottu@gmail.com>
2015-05-08 15:36:23 +0900

    implemented delete subcommand (wip)

--- source/app.d
+++ source/app.d
@@ -8,11 +8,13 @@ enum SubCommand : string
     Create = "create",
     Delete = "delete",
     Start = "start",
-    Stop = "stop"
+    Stop = "stop",
+    List = "list"
 }
 
 int main( string[] args )
 {
+    string machinectl = "machinectl";
     string pacstrap = "pacstrap";
     string nspawn = "systemd-nspawn";
     string container_root = "/var/lib/container/";
@@ -53,13 +55,19 @@ int main( string[] args )
             assert( exists( container_path ), "%s not found.".format(container_path) );
             remove( container_path ~ "/etc/securetty" );
 
-            auto temp = execute( ["machinectl", "enable", container_name] );
-            temp = execute( ["machinectl", "start", container_name] );
+            execute( [machinectl, "enable", container_name] );
+            execute( [machinectl, "start", container_name] );
         } break;
 
         case SubCommand.Delete:
         {
+            string container_name = args[2];
+            string container_path = container_root ~ container_name;
 
+            execute( [machinectl, "disable", container_name] );
+            execute( [machinectl, "stop", container_name] );
+
+            rmdirRecurse( container_path );
         } break;
 
         case SubCommand.Start:
@@ -70,7 +78,23 @@ int main( string[] args )
 
         case SubCommand.Stop:
         {
+            string container_name = args[2];
 
+        } break;
+
+        case SubCommand.List:
+        {
+            auto pid =
+                spawnProcess(
+                    [machinectl, "list"],
+                    std.stdio.stdin,
+                    std.stdio.stdout
+                );
+
+            if (wait(pid) != 0)
+            {
+                return 1;
+            }
         } break;
     }
 
